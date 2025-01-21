865ffbee32ce0915a41907e3d8aa2eadbbf29cdc
Manpreet Singh <manpreetsinghkainth25@gmail.com>
2025-01-21 09:28:38 +0530

    Initial commit

---
+++ app/src/main/java/com/neatroots/myshopping_app/SeeMoreActivity.kt
@@ -0,0 +1,11 @@
+package com.neatroots.myshopping_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeeMoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_see_more)
+    }
+}
